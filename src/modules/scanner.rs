40c3fe19dee465678ac148f61b89f27f352d1f0a
joydeep2899 <joydeep2899@gmail.com>
2024-02-03 16:23:16 +0530

    Adding Support for reading magic numbers
    Adding support for reading file magic numbers
    closes #4

--- src/modules/scanner.rs
+++ src/modules/scanner.rs
@@ -3,17 +3,32 @@ use std::fs;
 use std::hash::{Hash, Hasher};
 use std::path::Path;
 
+///get magic number from a file
+fn file_magic(data: &Vec<u8>) -> Vec<u8> {
+    let magic: Vec<u8> = vec![data[0], data[1], data[2], data[3]];
+    return magic;
+}
 
-/// func to calculate hash of Data 
+/// func to calculate hash of Data
 fn hash_data(data: &Vec<u8>) -> u64 {
     let mut hasher = DefaultHasher::new();
     data.hash(&mut hasher);
+    print!("file magic :{:#x?}", file_magic(data));
     return hasher.finish();
 }
 
-/// func to scan file 
+/// func to scan file
 fn scan_file(filepath: &Path) -> () {
-    let data = fs::read(filepath).unwrap();
+    let mut data: Vec<u8> = vec![];
+    match fs::read(filepath) {
+        Ok(d) => {
+            data = d;
+        }
+        Err(err) => {
+            print!("Error reading file {}:{}", filepath.display(), err)
+        }
+    };
+
     if let Ok(metadata) = filepath.metadata() {
         let hash = hash_data(&data);
 
@@ -24,6 +39,8 @@ fn scan_file(filepath: &Path) -> () {
             metadata.len()
         );
         println!("Hash is {:}", hash);
+    } else {
+        print!("Error");
     }
 }
 
@@ -50,8 +67,6 @@ pub fn scan(dir_path: &Path) {
             }
         }
     } else {
-        let pathbuf = dir_path.to_str().unwrap();
-        println!("Could Not Read Path : {pathbuf}");
+        scan_file(dir_path);
     }
 }
-
